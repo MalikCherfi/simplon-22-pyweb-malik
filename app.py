4b8aeffd1949a74fa9d288af80161b5734e4e570
malikcherfi <malikcherfi@gmail.com>
2026-06-23 15:22:27 +0200

    fix : disabled bandit check for host and remove deubg mod

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ if __name__ == "__main__":
     if not os.path.exists(LOG_FOLDER):
         os.makedirs(LOG_FOLDER)
 
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=False)  # nosec B104
